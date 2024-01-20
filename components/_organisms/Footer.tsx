77c6ced7e2be37c1db36fbfcfc8c2f80f7ab75e3
Avinash Rao <71743844+avinash7rao@users.noreply.github.com>
2024-01-20 11:26:10 -0500

    Update Footer.tsx

--- components/_organisms/Footer.tsx
+++ components/_organisms/Footer.tsx
@@ -8,16 +8,6 @@ interface FooterProps {}
 const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className='rounded-lg'>
-      <div className='w-full max-w-screen-xl mx-auto p-4 md:py-8 m-4'>
-        <hr className='my-6 sm:mx-auto dark:border-gray-700 lg:my-8' />
-        <span className='block text-sm text-gray-500 sm:text-center dark:text-gray-400'>
-          © 2024&nbsp;
-          <a href='https://avirao.vercel.app/' className='hover:underline'>
-            Avinash Rao
-          </a>
-          . All Rights Reserved.
-        </span>
-      </div>
       <div className='py-3 flex justify-center space-x-4'>
         <a
           href='https://www.linkedin.com/in/avinash-k-rao/'
@@ -44,6 +34,16 @@ const Footer: React.FC<FooterProps> = () => {
           rel='noopener noreferrer'>
           <ImYoutube className='text-4xl text-teal-500 hover:text-teal-600 cursor-pointer' />
         </a>
+      </div>
+      <div className='w-full max-w-screen-xl mx-auto p-4 md:py-8 m-4'>
+        <hr className='my-6 sm:mx-auto dark:border-gray-700 lg:my-8' />
+        <span className='block text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+          © 2024&nbsp;
+          <a href='https://avirao.vercel.app/' className='hover:underline'>
+            Avinash Rao
+          </a>
+          . All Rights Reserved.
+        </span>
       </div>
     </footer>
   );
