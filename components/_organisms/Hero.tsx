d22678fb8ec084973975ee30c7c0761649f3dad3
avinash7rao <avinash7rao@gmail.com>
2024-01-28 21:50:18 -0500

    update data folder and removed hardcode

--- components/_organisms/Hero.tsx
+++ components/_organisms/Hero.tsx
@@ -1,17 +1,54 @@
-import React from "react";
-import myAvatar from "@/public/images/my-avatar.png";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import ButtonPill from "../_atoms/buttons/button-pill";
 import Section from "../_templates/Section";
 
-interface HeroProps {}
+interface HeroProps {
+  info: {
+    name: string;
+    position: string;
+    summary: string;
+    avatar: {
+      src: string;
+      alt: string;
+    };
+  };
+}
 
 const Hero: React.FC<HeroProps> = () => {
+  const [data, setData] = useState<HeroProps | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/data");
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  const { info } = data;
+
   return (
     <Section id='about'>
       <div className='pb-10 flex flex-col lg:flex-row items-center justify-center gap-15'>
         <div className='mx-10 bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96 lg:flex-end'>
-          <Image src={myAvatar} alt='Avinash`s avatar' />
+          <Image
+            src={info?.avatar.src || ""}
+            alt={info?.avatar.alt || ""}
+            width={500}
+            height={500}
+            priority
+          />
         </div>
         <div className='text-center pt-16'>
           <div className='lg:flex-start'>
@@ -19,10 +56,10 @@ const Hero: React.FC<HeroProps> = () => {
               Hello I&apos;m
             </h3>
             <h2 className='text-5xl py-4 text-teal-600 font-medium dark:text-teal-400 md:text-6xl'>
-              Avinash Rao
+              {info?.name}
             </h2>
             <h3 className='text-2xl py-2 dark:text-white md:text-3xl'>
-              Developer & Designer
+              {info?.position}
             </h3>
           </div>
           <div className='flex flex-row gap-3 py-6 justify-center'>
