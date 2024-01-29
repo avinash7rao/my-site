6968daa58cb159a8aa4846720e8973c7be13c278
avinash7rao <avinash7rao@gmail.com>
2024-01-29 01:12:51 -0500

    updated minor styling and data changes

--- components/_atoms/Title.tsx
+++ components/_atoms/Title.tsx
@@ -7,7 +7,7 @@ interface Props {
 const Title: React.FC<Props> = ({ title }) => {
   return (
     <div>
-      <h3 className='text-3xl font-bold mb-4 text-center py-4 rounded-lg backdrop-blur-md bg-white bg-opacity-25 lg:bg-inherit lg:py-10'>
+      <h3 className='text-4xl font-bold mb-4 text-center py-4 rounded-lg backdrop-blur-md bg-white bg-opacity-25 lg:bg-inherit lg:py-10'>
         {title}
       </h3>
     </div>
